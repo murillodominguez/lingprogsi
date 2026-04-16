b2c64695005ec5049a6fe8c2368c90b69d0c8dbe
murillodominguez <murillo.v.carballo@gmail.com>
2026-04-16 15:04:20 -0300

    Atualização Exercício Ponteiros

--- bimestre-01/aula_04-09/c/produtos.c
+++ bimestre-01/aula_04-09/c/produtos.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct Produto {
     char nome[30];
@@ -12,24 +13,21 @@ typedef struct Node {
     struct Node *proximo;
 } Node;
 
-void adicionarProduto(Produto *p);
+void adicionarProduto(Node *head);
 
-int checarExistencia(Produto *p);
+int checarExistencia(Node *head);
 
-void exibirProdutos(Produto *p);
+void exibirProdutos(Node *head);
 
-void buscaProduto(Produto *p);
+void buscaProduto(Node *head);
+
+void liberarEspaco(Node *head);
 
 int main() {
     printf("Bem-vindo ao Gestor de Produtos!\n\n");
 
     int loop = 1;
-    Produto *Produtos = NULL;
-    
-    if (!Produtos) {
-        printf("ERRO: Memoria Insuficiente");
-        exit(1);
-    }
+    Node *head = NULL;
 
     while (loop) {
         printf("Digite uma das opções do menu para prosseguir:\n\n");
@@ -45,13 +43,13 @@ int main() {
 
         switch(escolha){
             case 1:
-                adicionarProduto(Produtos);
+                adicionarProduto(head);
                 break;
             case 2:
-                exibirProdutos(Produtos);
+                exibirProdutos(head);
                 break;
             case 3:
-                buscaProduto(Produtos);
+                buscaProduto(head);
                 break;
             case 4:
                 loop = 0;
@@ -62,53 +60,70 @@ int main() {
         }
     }
 
+    liberarEspaco(head);
+
     return 0;
 }
 
-void adicionarProduto(Node **p) {
+void adicionarProduto(Node *head) {
     char nome[30];
     int codigo;
     double preco;
-    Node *novoProduto = (Node*)malloc(sizeof(Node));
+    Node *novoProdutoPtr = (Node*)malloc(sizeof(Node));
 
-    if (!novoProduto) {
+    if (!novoProdutoPtr) {
         printf("ERRO: Memoria Insuficiente");
         exit(1);
     }
 
     printf("\nInforme o nome do produto a adicionar: ");
-    scanf("%s", nome);
+    scanf("%s", novoProdutoPtr->prod.nome);
 
     printf("\n\nInforme o código do produto: ");
-    scanf("%d", &codigo);
+    scanf("%d", &novoProdutoPtr->prod.codigo);
 
     printf("\n\nInforme o preço do produto: ");
-    scanf("%lf", &preco);
+    scanf("%lf", &novoProdutoPtr->prod.preco);
 
-    novoProduto->nome = nome;
-    novoProduto->codigo = codigo;
-    novoProduto->preco = preco;
-    novoProduto->proximo = NULL;
+    novoProdutoPtr->proximo = NULL;
 
-    if (*p == NULL) {
-        *p = novoProduto;
-        return
+    if (head == NULL) {
+        head = novoProdutoPtr;
+        printf("primeiro endereço: %p", head);
+        return;
     }
 
-    Node *temp = *p;
-
-    while (temp->proximo != NULL) {
-        temp = temp->next;
-    }
-
-    temp->next = novoProduto;
-}
-
-void exibirProdutos(Node *p) {
-    Produto *atual = p;
+    Node *temp = head;
     
+    while (temp->proximo != NULL) {
+        temp = temp->proximo;
+    }
+
+    temp->proximo = novoProdutoPtr;
+
+    printf("endereço temp: %p", temp->proximo);
 }
 
-void buscaProduto(Produto *p) {
+void exibirProdutos(Node *head) {
+    Node *atual = head;
+    while (atual != NULL) {
+        printf("\nProduto %d \nNome: %s\nPreço: %.2lf",  atual->prod.codigo, atual->prod.nome, atual->prod.preco);
+        atual = atual->proximo;
+    }
+}
+
+void buscaProduto(Node *head) {
     printf("Buscar produto");
+}
+
+void liberarEspaco(Node *head) {
+    if (head != NULL) {
+        free(head);
+        if (head->proximo != NULL) {
+            Node *temp = head;
+            while (temp->proximo != NULL) {
+                free(temp);
+            }
+        }
+    }
 }
